67dde10565142d509e84eecbbe499075823e8639
linanzhi <linanzhi@longshine.com>
2022-04-02 13:26:47 +0800

    init

---
+++ .eslintrc.js
@@ -0,0 +1,25 @@
+module.exports = {
+  env: {
+    browser: true,
+    commonjs: true,
+    es2021: true,
+    node: true,
+    // 开启setup语法糖环境
+    'vue/setup-compiler-macros': true,
+  },
+  extends: [
+    'standard',
+    // 'plugin:vue/essential',
+    'plugin:vue/vue3-recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:prettier/recommended',
+  ],
+  parser: 'vue-eslint-parser',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    parser: '@typescript-eslint/parser',
+    sourceType: 'module',
+  },
+  plugins: ['vue', '@typescript-eslint'],
+  rules: {},
+};
